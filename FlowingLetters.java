7a8dc401c019a8e026b0f158a7be310090337886
Lake Biwa <stickfigure.work@gmail.com>
2024-07-26 16:06:39 +0900

    Add files via upload

---
+++ FlowingLetters.java
@@ -0,0 +1,23 @@
+// 使い方
+// コンパイルできたらjava Main <対象の文字列> <間隔数(0に近いほど動かすとなめらかになります)> <繰り返す回数>
+
+public class FlowingLetters{
+    public static void main(String[] args) {
+        String target = args[0];
+        int intervalNum = Integer.parseInt(args[1]);
+        int repeatNum = Integer.parseInt(args[2]);
+        StringBuilder interval = new StringBuilder();
+        for (int i = 0; i < intervalNum; i++){
+            interval.append(" ");
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(target);
+        System.out.println(sb.toString());
+        for (int i = 0; i < repeatNum - 1; i++){
+            System.out.println(sb.insert(0, interval).toString());
+        }
+        for (int i = 0; i < repeatNum - 1; i++){
+            System.out.println(sb.delete(0, intervalNum));
+        }
+    }
+}
